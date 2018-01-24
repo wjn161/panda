ecb0b1ddc1ced5f629e57181ebed65d85d6f1a31
wjn161 <wjn-161@163.com>
2018-01-24 23:15:55 +0800

    update worker id assigner

--- panda-shardingjdbc/src/main/java/me/wujn/panda/shardingjdbc/idgen/worker/ReuseableWorkerIdAssigner.java
+++ panda-shardingjdbc/src/main/java/me/wujn/panda/shardingjdbc/idgen/worker/ReuseableWorkerIdAssigner.java
@@ -4,8 +4,15 @@
  */
 package me.wujn.panda.shardingjdbc.idgen.worker;
 
+import me.wujn.panda.shardingjdbc.idgen.utils.NetUtils;
+import me.wujn.panda.shardingjdbc.idgen.utils.SystemClock;
+import org.apache.commons.lang3.RandomUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
 
 /**
  * @author wujn
@@ -14,12 +21,26 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReuseableWorkerIdAssigner implements WorkerIdAssigner {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReuseableWorkerIdAssigner.class);
     @Autowired
     private WorkerNodeRepository workerNodeRepository;
 
     @Override
     public long assignWorkerId() {
-        return 0;
+        try {
+            String hostName = NetUtils.getLocalInetAddress().getHostName();
+            String port = "0";//String.valueOf(SystemClock.millisClock().now() + RandomUtils.nextInt(0, 10000));
+            WorkerNode workerNode = workerNodeRepository.get(hostName, port);
+            if (workerNode == null) {
+                //insert a worker node
+                workerNode = new WorkerNode();
+                workerNodeRepository.insert(workerNode);
+            }
+            return Long.valueOf(workerNode.getWorkerId());
+        } catch (Exception ex) {
+            LOGGER.error("worker id assign error", ex);
+            return 0;
+        }
     }
 
     @Override
